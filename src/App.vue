bd727f031bec0ebe9fb69c5ed54935153ef904b1
marcotrunc <marcotrunc@gmail.com>
2022-02-08 15:57:44 +0100

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="app"></div>
+  <div>
+    <Header />
+    <main>
+      <main-top></main-top>
+      <main-bottom></main-bottom>
+    </main>
+    <footer>
+      <footer-top></footer-top>
+      <footer-bottom></footer-bottom>
+    </footer>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style lang="scss">
+@import "./assets/sass/style.scss";
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container-fluid">
+    <header class="container">
+      <div id="header-left">
+        <a href="#">
+          <img src="../assets/img/dc-logo.png" alt="" />
+        </a>
+      </div>
+      <div id="header-right">
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#" class="active">Comics</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">Collectibles</a></li>
+          <li><a href="#">Videos</a></li>
+          <li><a href="#">Fans</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Shop</a></li>
+        </ul>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/sass/_vars.scss";
+header {
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: $bg_white;
+}
+#header-left {
+  width: 5%;
+}
+#header-right {
+  ul li {
+    list-style-type: none;
+    display: inline-block;
+    padding: 0 10px;
+    a {
+      text-decoration: none;
+      text-transform: uppercase;
+      padding: 30px 0;
+      color: black;
+      font-size: 0.7rem;
+      &:hover,
+      &:active {
+        color: $color_blue;
+        border-bottom: 3px solid $color_blue;
+      }
+    }
+  }
+}
+</style>
